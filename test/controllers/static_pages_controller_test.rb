149f877746dced9b388b6e886de5a821d85d8948
len lee <lenlee454@gmail.com>
2016-11-14 14:24:24 -0800

    Finish static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get root" do
-    get static_pages_home_url
+    get '/'
     assert_response :success
   end
   
