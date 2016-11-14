3f321d561e7ce271232557dd283967990566b1f2
len lee <lenlee454@gmail.com>
2016-11-14 13:53:46 -0800

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  
   root 'application#hello'
 end
